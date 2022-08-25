1c0e863df44d042918af9c4f5887f9de3e7be2df
eduardo-zambrano <eduardo@thewayofit.com>
2022-08-25 13:50:59 -0700

    With test

---
+++ test.jl
@@ -0,0 +1,13 @@
+""" 
+Source:
+https://julia.quantecon.org/getting_started_julia/julia_by_example.html
+"""
+
+using Plots
+
+n = 100
+f(x) = x^2
+ϵ = randn(n)
+plot(f.(ϵ), label="ϵ²")
+plot!(ϵ, label="ϵ")
+
