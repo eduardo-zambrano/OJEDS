b574595633926f0847b464cf7362d845305b7d7f
eduardo-zambrano <eduardo@thewayofit.com>
2022-09-21 15:22:03 -0700

    Changed "activate ."

--- Lecture_1_REPL.jl
+++ Lecture_1_REPL.jl
@@ -1,10 +1,15 @@
-activate .
+using Pkg
+Pkg.activate(".")
+
 include("REPL_helper.jl")
+
 using Plots
 n = 100
 f(x) = x^2
 ϵ = randn(n)
 plot(f.(ϵ),label="ϵ²")
 plot!(ϵ,label="ϵ")
+
 include("first_LP.jl")
+
 save_REPL_history("Lecture_1_REPL.jl")
