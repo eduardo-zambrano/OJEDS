a1f8597b012bfc4c48363c08807f1a95e29d9a17
eduardo-zambrano <eduardo@thewayofit.com>
2022-09-22 12:45:59 -0700

    Lecture 2 REPL Session Log

---
+++ Lecture_2_REPL.jl
@@ -0,0 +1,29 @@
+# Run Julia from your OJEDS directory
+# ]
+# activate .
+# <BACKSPACE>
+include("REPL_helper.jl")
+using OhMyREPL
+x = 3 + 3
+ans
+
+β = 9
+⛵ = 10
+# ;
+# pwd
+# ls
+# ?
+# sin
+# ;
+# vim README.md
+# Stuff happened inside vim
+# ls
+#git add README.md
+# git commit -m "Incorporated Christine's taste in music"
+#git push
+# Enter ]
+# status
+# gc
+
+using UnicodePlots
+save_REPL_history("Lecture_2_REPL.jl")
