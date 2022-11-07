7c99957838274e419fb3bb65832ff755413328ed
eduardo-zambrano <eduardo@thewayofit.com>
2022-11-07 12:51:37 -0800

    Fixed Lecture 15 notes

--- L15_notes.jl
+++ L15_notes.jl
@@ -17,15 +17,17 @@ R = [0.97 1.86 0.41;
 m, n = size(R);
 vdes = 1e3 * ones(m);
 
+# The unconstrained solution
 x_unc = R \ vdes
 
+# The matrix and vector that define the constraint
 S = [1 0 ; 0 1 ; -1 -1 ]
 s = [0 ; 0 ; 1284]
 
+# The modified data
 A_R = R*S
-
 vdes_R = vdes - R*s
 
+# The constrained solution
 x_R = A_R \ vdes_R
-
 x = S*x_R + s
