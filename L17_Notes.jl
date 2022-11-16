a8796adce03887bfabc681e9be13acd714ee8d96
eduardo-zambrano <eduardo@thewayofit.com>
2022-11-16 14:54:29 -0800

    Streamlined Portfolio Optimization code

--- L17_Notes.jl
+++ L17_Notes.jl
@@ -36,19 +36,22 @@ R, Rtest = portfolio_data();
 T, n = size(R)
 Ttest, n = size(Rtest)
 
-rho = 0.10/250; # Ask for 10% annual return
-w = port_opt(R,rho);
+ρ = 0.10/250; # Ask for 10% annual return
+w = port_opt(R,ρ);
 r = R*w; # Portfolio return time series
-pf_return = 250*mean(r)
+pf_return = 250*mean(r) #Average annual return
 
 # Plot the in-sample behavior of the porfolio
-plot(1:T, cum_value(r), label= "10%", legend=:topleft)
+plot(1:T, cum_value(r), label= "the 10% portfolio", legend=:topleft)
 
 # How do we do out of sample?
-rtest = Rtest*w; # Portfolio return time series
-pf_return_test = 250*mean(rtest)
+rtest = Rtest * w; # Portfolio return time series
+pf_return_test = 250 * mean(rtest)
+plot(1:Ttest, cum_value(ρ*ones(Ttest)), label= "'steady' 10%")
+plot!(1:Ttest, cum_value(rtest), label= "the 10% portfolio underperforms out of sample!", legend=:topleft)
 
 # Problem: Markowitz's curse
+
 """
 Solution: Nested Clustered Optimization
 """
@@ -58,7 +61,7 @@ Solution: Nested Clustered Optimization
 # # clustering algorithm. Here, I used k-means clustering, as in Chapter 4 of our textbook, with k=5.
 """
 
-# k = 5;
+k = 5;
 
 # # Prepare the data for clustering
 # D = sqrt.(0.5* (ones(size(n)) .- cor(R)))
@@ -110,66 +113,46 @@ Solution: Nested Clustered Optimization
 # kmeans_(x, k; maxiters = 100, tol = 1e-5)
 
 # The algorithm produces an assignment of each column of x to a cluster
-# [3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2]
+D = [3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2]
 
 """
 # Step 2: perform portfolio optimization separately in each cluster
 """
 
 # Create the five separate groups of assets
-R1 = R[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 1]
-R2 = R[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 2]
-R3 = R[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 3]
-R4 = R[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 4]
-R5 = R[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 5]
+P = [ R[:,D .== i] for i in 1:k ];
 
 # Optimize inside each cluster
-w1 = port_opt(R1, rho);
-w2 = port_opt(R2, rho);
-w3 = port_opt(R3, rho);
-w4 = port_opt(R4, rho);
-w5 = port_opt(R5, rho);
+wₚ = [ port_opt(P[i], ρ) for i in 1:k ];
 
 # Track in-sample returns 
-Opt_R1 = R1 * w1
-Opt_R2 = R2 * w2
-Opt_R3 = R3 * w3
-Opt_R4 = R4 * w4
-Opt_R5 = R5 * w5
-opt_R = [Opt_R1 Opt_R2 Opt_R3 Opt_R4 Opt_R5]
+rₚ = [ P[i] * wₚ[i] for i in 1:k ];
+Pᵣ = hcat(rₚ...);
 
 """
 # Step 3: Create a portfolio of portfolios
 """
+wₚₚ = port_opt(Pᵣ,ρ);
+rₚₚ = Pᵣ * wₚₚ; # Portfolio return time series
+pf_return = 250*mean(rₚₚ)
 
-w_pp = port_opt(opt_R,rho);
-r_pp = opt_R*w_pp; # Portfolio return time series
-pf_return = 250*mean(r_pp)
-
-# Prepare to track out-of-sample returns 
+## Prepare to track out-of-sample returns 
 
 # Split the test data into the same five clusters
-Rtest1 = Rtest[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 1]
-Rtest2 = Rtest[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 2]
-Rtest3 = Rtest[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 3]
-Rtest4 = Rtest[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 4]
-Rtest5 = Rtest[:,[3, 5, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 5, 4, 3, 1, 3, 1, 2] .== 5]
-
+Ptest = [ Rtest[:,D .== i] for i in 1:k ];
 
 # Track the returns of the five cluster-optimal portfolios out of sample
-Opt_Rtest1 = Rtest1 * w1
-Opt_Rtest2 = Rtest2 * w2
-Opt_Rtest3 = Rtest3 * w3
-Opt_Rtest4 = Rtest4 * w4
-Opt_Rtest5 = Rtest5 * w5
-opt_Rtest = [Opt_Rtest1 Opt_Rtest2 Opt_Rtest3 Opt_Rtest4 Opt_Rtest5]
+rₚ_test = [ Ptest[i] * wₚ[i] for i in 1:k ];
+Pᵣ_test = hcat(rₚ_test...);
 
 # Track the returns of the portfolio of portfolios
-r_pp_test = opt_Rtest*w_pp
+rₚₚ_test = Pᵣ_test * wₚₚ 
+
+# pf_return_test = 250 * mean(rₚₚ_test)
 
 # Compare with the  out of sample performance of the original portfolio
 plot(1:Ttest, cum_value(rtest), label= "Original", legend=:topleft)
-plot!(1:Ttest, cum_value(r_pp_test), label= "NCO")
+plot!(1:Ttest, cum_value(rₚₚ_test), label= "NCO")
 
 # Remember to 
 # 1. save the REPL session.
